f8d7cfd79d245fe7527e985fc13fffac70b29ce0
blaire-pi <gpwls1969@cau.ac.kr>
2023-01-06 06:52:00 +0900

    초대코드뷰 - 버튼 비활성화, 붙여넣기

--- EndOfDay/EndOfDay/View/Main/EnterCodeView.swift
+++ EndOfDay/EndOfDay/View/Main/EnterCodeView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+//import UniformTypeIdentifiers
 
 struct EnterCodeView: View {
     private enum Field : Int, Hashable{
@@ -17,7 +18,14 @@ struct EnterCodeView: View {
     
     @EnvironmentObject var diaryStore: DiaryStore
     @FocusState private var focusField : Field?
+    
+//    @State private var pasteboard: String = ""
     //focusField = .addAttendee
+    
+    
+    var trimContent: String {
+        codeText.trimmingCharacters(in: .whitespaces)
+    }
     
     var body: some View {
         NavigationStack{
@@ -32,19 +40,34 @@ struct EnterCodeView: View {
                             .focused($focusField, equals: .addAttendee)
                     )
                 
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 340, height: 50)
-                    .foregroundColor(Color("LightGray"))
-                    .overlay(
-                        Text("완료")
-                    )
-                    .onTapGesture {
-                        Task {
-                            await diaryStore.joinDiary(diaryID: codeText)
-                        }
-                        print("코드 제출 완료")
-                        showingSheet.toggle()
+                Button {
+                    Task {
+                        await diaryStore.joinDiary(diaryID: codeText)
                     }
+                    print("코드 제출 완료")
+                    showingSheet.toggle()
+                } label: {
+                    Text("완료")
+                        .frame(width: 340, height: 50)
+                        .background(Color("LightGray"))
+                        .cornerRadius(10)
+                    
+                }
+                .disabled(trimContent.count > 0 ? false : true)
+                
+                //                RoundedRectangle(cornerRadius: 10)
+                //                    .frame(width: 340, height: 50)
+                //                    .foregroundColor(Color("LightGray"))
+                //                    .overlay(
+                //                        Text("완료")
+                //                    )
+                //                    .onTapGesture {
+                //                        Task {
+                //                            await diaryStore.joinDiary(diaryID: codeText)
+                //                        }
+                //                        print("코드 제출 완료")
+                //                        showingSheet.toggle()
+                //                    }
             }
             .onAppear {
                 focusField = .addAttendee
@@ -62,6 +85,14 @@ struct EnterCodeView: View {
                 ToolbarItem(placement: .navigation) {
                     Text("초대 코드 입력")
                         .frame(width: 280, alignment: .center)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Text("붙여넣기")
+                        .onTapGesture {
+                            if let str = UIPasteboard.general.string {
+                                codeText = str
+                            }
+                        }
                 }
             }
         }
