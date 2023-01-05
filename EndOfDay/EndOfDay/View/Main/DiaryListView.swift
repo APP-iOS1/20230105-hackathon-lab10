fb312c7fd181b1802e2393705459f54971ca5cd1
jaehee6 <0jujh8@gmail.com>
2023-01-06 05:48:29 +0900

    calendar 점찍기, 토글버튼, 애니메이션 제거

--- EndOfDay/EndOfDay/View/Main/DiaryListView.swift
+++ EndOfDay/EndOfDay/View/Main/DiaryListView.swift
@@ -13,6 +13,7 @@ struct DiaryListView: View {
     @StateObject private var recordStore: RecordStore = RecordStore()
     @State var currentDate: Date = Date()
     var diary: Diary
+    @State var isShowingCalendar: Bool = false
     
     var body: some View {
         NavigationStack {
@@ -21,16 +22,24 @@ struct DiaryListView: View {
                     Text("그룹명")
                         .font(.largeTitle)
                     Spacer()
+                    Button {
+                        isShowingCalendar.toggle()
+                    } label: {
+                        Image(systemName: "calendar")
+                        Text("\(currentDate.formatted(.dateTime.day().month()))")
+                    }
+
                 }
                 .padding(.horizontal)
                 
                 ScrollView(.vertical, showsIndicators: false) {
                     VStack(spacing: 20) {
                         // Custom Date Picker
-                        CustomDatePicker(currentDate: $currentDate, diaryID: diary.id)
+                        CustomDatePicker(isShowingCalendar: $isShowingCalendar, currentDate: $currentDate, diaryID: diary.id)
                     }
                     .padding(.vertical)
                 }
+                .padding(.top, -20)
                 
 
 //                ScrollView {
@@ -43,6 +52,7 @@ struct DiaryListView: View {
 //                    }
 //                }
             }
+           
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     NavigationLink {
@@ -70,8 +80,8 @@ struct DiaryListView: View {
     }
 }
 
-//struct DiaryListView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        DiaryListView()
-//    }
-//}
+struct DiaryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryListView(diary: Diary(id: "dd", dairyTitle: "타이틀", colorIndex: 1, createdAt: 1, membersID: [], membersNickname: []))
+    }
+}
