88e92b0f78a350234f262f32187ed8555bf5f6cd
Lee-Youngwoo <lyw2072@naver.com>
2023-01-05 17:26:35 +0900

    회원가입뷰 수정
    회원가입뷰 수정

--- EndOfDay/EndOfDay/View/Login/SignUpView.swift
+++ EndOfDay/EndOfDay/View/Login/SignUpView.swift
@@ -45,7 +45,20 @@ struct SignUpView: View {
     var body: some View {
         NavigationStack {
             VStack(alignment:.leading) {
-                Text("이메일")
+                // MARK: 이메일
+                HStack{
+                    Text("이메일")
+                    Spacer()
+                    if !emailID.isEmpty && !isCheckValidEmail {
+                        Text("이메일 형식이 맞지 않습니다")
+                            .foregroundColor(.red)
+                    } else if !emailID.isEmpty && isCheckValidEmail {
+                        Text("사용가능한 이메일입니다")
+                            .foregroundColor(.blue)
+                    }else if emailID.isEmpty{
+                        Text(" ")
+                    }
+                }
                 TextField("이메일", text: $emailID)
                     .padding()
                     .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
@@ -55,7 +68,20 @@ struct SignUpView: View {
                     .keyboardType(.emailAddress)
                     .focused($focusField, equals: .emailID)
                 
-                Text("닉네임")
+                // MARK: 닉네임
+                HStack{
+                    Text("닉네임")
+                    Spacer()
+                    if !nickname.isEmpty && !(nickname.count > 1 && nickname.count < 9){
+                        Text("닉네임 형식이 맞지 않습니다")
+                            .foregroundColor(.red)
+                    }else if !nickname.isEmpty && (nickname.count > 1 && nickname.count < 9){
+                        Text("사용가능한 닉네임입니다")
+                            .foregroundColor(.blue)
+                    }else if nickname.isEmpty{
+                        Text(" ")
+                    }
+                }
                 TextField("닉네임 : 2~8자", text: $nickname)
                     .padding()
                     .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
@@ -63,31 +89,75 @@ struct SignUpView: View {
                     .disableAutocorrection(true)
                     .focused($focusField, equals: .nickname)
                 
-                Text("비밀번호")
+                // MARK: 비밀번호
+                HStack{
+                    Text("비밀번호")
+                    Spacer()
+                    if !password.isEmpty && !(password.count > 7) {
+                        Text("비밀번호 형식이 맞지 않습니다")
+                            .foregroundColor(.red)
+                    } else if !password.isEmpty && (password.count > 7) {
+                        Text("사용가능한 비밀번호입니다")
+                            .foregroundColor(.blue)
+                    } else if password.isEmpty{
+                        Text(" ")
+                    }
+                }
                 SecureField("8자 이상의 비밀번호", text: $password)
                     .padding()
                     .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
                     .textContentType(.password)
                     .keyboardType(.default)
                     .focused($focusField, equals: .password)
-                
-                Text("비밀번호 확인")
+        
+                // MARK: 비밀번호 확인
+                HStack{
+                    Text("비밀번호 확인")
+                    Spacer()
+                    if !passwordCheck.isEmpty && password != passwordCheck {
+                        Text("비밀번호가 일치하지 않습니다")
+                            .foregroundColor(.red)
+                    } else if !passwordCheck.isEmpty && password == passwordCheck {
+                        Text("비밀번호가 일치합니다")
+                            .foregroundColor(.blue)
+                    }else if passwordCheck.isEmpty{
+                        Text(" ")
+                    }
+                }
                 SecureField("비밀번호 확인", text: $passwordCheck)
                     .padding()
                     .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
                     .textContentType(.password)
                     .keyboardType(.default)
-
+                
             }.padding(20)
             
-            Button {
-                userStore.signUp(emailAddress: emailID, password: password, nickname: nickname)
-                dismiss()
-                
-            } label: {
-                Text("회원가입하기")
-                    .modifier(MaxWidthColoredButtonModifier(cornerRadius: 15))
+            
+            if !emailID.isEmpty && !nickname.isEmpty && !password.isEmpty && !passwordCheck.isEmpty && password == passwordCheck{
+                Button {
+                    userStore.signUp(emailAddress: emailID, password: password, nickname: nickname)
+                    dismiss()
+                    
+                } label: {
+                    Text("회원가입하기")
+                        .modifier(MaxWidthColoredButtonModifier(cornerRadius: 15))
+                }
+            }else{
+                Button{
+                    
+                } label: {
+                    Text("회원가입하기")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .bold()
+                        .background(Color.gray)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .padding(EdgeInsets(top: 5, leading: 20, bottom: 0, trailing: 20))
+                }
             }
+
             
         }.navigationTitle("회원가입")
     }
@@ -112,7 +182,7 @@ struct SignUpView_Previews: PreviewProvider {
 struct MaxWidthColoredButtonModifier: ViewModifier {
     var color: Color = Color("AccentColor")
     var cornerRadius: CGFloat
-
+    
     func body(content: Content) -> some View {
         content
             .font(.subheadline)
@@ -127,121 +197,121 @@ struct MaxWidthColoredButtonModifier: ViewModifier {
 }
 /*
  ScrollView {
-     VStack(alignment:.leading) {
-         
-         Text(guideMessage).font(.title)
-         // 아래에서 닉네임이 유효하여 isNicknameCount가 true가 되면 보여짐
-         if isNicknameCount {
-             Text("비밀번호").padding(.top, 10)
-             SecureField("8자 이상의 비밀번호", text: $password)
-                 .padding()
-                 .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
-                 .textContentType(.password)
-                 .keyboardType(.default)
-                 .focused($focusField, equals: .password)
-                 .onAppear {
-                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                         focusField = .password
-                     }
-                 }
-             
-             Text("비밀번호 확인").padding(.top, 10)
-             SecureField("비밀번호 확인", text: $passwordCheck)
-                 .padding()
-                 .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
-                 .textContentType(.password)
-                 .keyboardType(.default)
-             Text("닉네임").padding(.top, 10)
-         }
-         // 아래에서 이메일이 유효하여 isCheckValidEmail이 true가 되면 보여짐
-         if !isDuplicatedCheck {
-             TextField("닉네임 : 2~8자", text: $nickname)
-                 .padding()
-                 .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
-                 .textInputAutocapitalization(.never)
-                 .disableAutocorrection(true)
-                 .focused($focusField, equals: .nickname)
-                 .onAppear {
-                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                         focusField = .nickname
-                     }
-                 }
-             Text("이메일").padding(.top, 10)
-         }
-         
-         TextField("이메일", text: $emailID)
-             .padding()
-             .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
-             .textContentType(.emailAddress)
-             .textInputAutocapitalization(.never)
-             .disableAutocorrection(true)
-             .keyboardType(.emailAddress)
-             .focused($focusField, equals: .emailID)
-             .onAppear {
-                 // TextField가 나타나면 Focus를 이 필드로 옮겨서 키보드를 띄워줌
-                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                     focusField = .emailID
-                 }
-             }
-         if !isCheckValidEmail {
-             Text("\(Image(systemName: "exclamationmark.triangle")) 잘못된 이메일 형식입니다.")
-                 .foregroundColor(.gray)
-         }
-
-         // 맨 위에서 password의 길이와 일치 검사가 모두 true면 버튼 등장
-         if isValidPassword {
-             Button {
-                 userStore.signUp(emailAddress: emailID, password: password, nickname: nickname)
-                 dismiss()
-             } label: {
-                 Text("회원가입")
-                     .bold()
-                     .font(.title3)
-                     .foregroundColor(Color.white)
-                     .frame(maxWidth: .infinity, minHeight: 55)
-                     .background(.black)
-                     .cornerRadius(10)
-             }
-             .padding(.top)
-         }
-     }
-     .padding()
-     .toolbar {
-         ToolbarItem(placement: .keyboard) {
-             // MARK: - 키보드 툴바 버튼 구현
-             // 텍스트 필드들이 해당 내용에 맞는 유효성 검사를 마칠 때 마다 버튼과 guideMessage 내용이 변경됨
-             if isDuplicatedCheck {
-                 Button {
-                     isDuplicatedCheck = userStore.duplicateCheck(emailAddress: emailID)
-                     if !isCheckValidEmail && isDuplicatedCheck {
-                         guideMessage = "이메일을 입력해주세요."
-                     }
-                 } label: {
-                     Text("이메일 중복확인").font(.title2)
-                 }
-             } else if !isNicknameCount {
-                 Button {
-                     isNicknameCount = nickname.count >= 2 && nickname.count <= 8
-                     if isNicknameCount {
-                         guideMessage = "비밀번호를 입력해주세요."
-                     }
-                 } label: {
-                     Text("닉네임 확인").font(.title2)
-                 }
-             } else if !isValidPassword {
-                 Button {
-                     isValidPassword = password.count >= 8 && password == passwordCheck
-                     if isValidPassword {
-                         guideMessage = "회원가입을 눌러주세요."
-                         focusField = nil
-                         UIApplication.shared.endEditing()
-                     }
-                 } label: {
-                     Text("비밀번호 확인").font(.title2)
-                 }
-             }
-         }
-     }
+ VStack(alignment:.leading) {
+ 
+ Text(guideMessage).font(.title)
+ // 아래에서 닉네임이 유효하여 isNicknameCount가 true가 되면 보여짐
+ if isNicknameCount {
+ Text("비밀번호").padding(.top, 10)
+ SecureField("8자 이상의 비밀번호", text: $password)
+ .padding()
+ .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
+ .textContentType(.password)
+ .keyboardType(.default)
+ .focused($focusField, equals: .password)
+ .onAppear {
+ DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+ focusField = .password
+ }
+ }
+ 
+ Text("비밀번호 확인").padding(.top, 10)
+ SecureField("비밀번호 확인", text: $passwordCheck)
+ .padding()
+ .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
+ .textContentType(.password)
+ .keyboardType(.default)
+ Text("닉네임").padding(.top, 10)
+ }
+ // 아래에서 이메일이 유효하여 isCheckValidEmail이 true가 되면 보여짐
+ if !isDuplicatedCheck {
+ TextField("닉네임 : 2~8자", text: $nickname)
+ .padding()
+ .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
+ .textInputAutocapitalization(.never)
+ .disableAutocorrection(true)
+ .focused($focusField, equals: .nickname)
+ .onAppear {
+ DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+ focusField = .nickname
+ }
+ }
+ Text("이메일").padding(.top, 10)
+ }
+ 
+ TextField("이메일", text: $emailID)
+ .padding()
+ .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
+ .textContentType(.emailAddress)
+ .textInputAutocapitalization(.never)
+ .disableAutocorrection(true)
+ .keyboardType(.emailAddress)
+ .focused($focusField, equals: .emailID)
+ .onAppear {
+ // TextField가 나타나면 Focus를 이 필드로 옮겨서 키보드를 띄워줌
+ DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+ focusField = .emailID
+ }
+ }
+ if !isCheckValidEmail {
+ Text("\(Image(systemName: "exclamationmark.triangle")) 잘못된 이메일 형식입니다.")
+ .foregroundColor(.gray)
+ }
+ 
+ // 맨 위에서 password의 길이와 일치 검사가 모두 true면 버튼 등장
+ if isValidPassword {
+ Button {
+ userStore.signUp(emailAddress: emailID, password: password, nickname: nickname)
+ dismiss()
+ } label: {
+ Text("회원가입")
+ .bold()
+ .font(.title3)
+ .foregroundColor(Color.white)
+ .frame(maxWidth: .infinity, minHeight: 55)
+ .background(.black)
+ .cornerRadius(10)
+ }
+ .padding(.top)
+ }
+ }
+ .padding()
+ .toolbar {
+ ToolbarItem(placement: .keyboard) {
+ // MARK: - 키보드 툴바 버튼 구현
+ // 텍스트 필드들이 해당 내용에 맞는 유효성 검사를 마칠 때 마다 버튼과 guideMessage 내용이 변경됨
+ if isDuplicatedCheck {
+ Button {
+ isDuplicatedCheck = userStore.duplicateCheck(emailAddress: emailID)
+ if !isCheckValidEmail && isDuplicatedCheck {
+ guideMessage = "이메일을 입력해주세요."
+ }
+ } label: {
+ Text("이메일 중복확인").font(.title2)
+ }
+ } else if !isNicknameCount {
+ Button {
+ isNicknameCount = nickname.count >= 2 && nickname.count <= 8
+ if isNicknameCount {
+ guideMessage = "비밀번호를 입력해주세요."
+ }
+ } label: {
+ Text("닉네임 확인").font(.title2)
+ }
+ } else if !isValidPassword {
+ Button {
+ isValidPassword = password.count >= 8 && password == passwordCheck
+ if isValidPassword {
+ guideMessage = "회원가입을 눌러주세요."
+ focusField = nil
+ UIApplication.shared.endEditing()
+ }
+ } label: {
+ Text("비밀번호 확인").font(.title2)
+ }
+ }
+ }
+ }
  }
  Spacer()
  */
